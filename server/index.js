7c3eebdb0d9c95b384c37afffb6318a6f418fc63
Safa Khan <asafa2610@gmail.com>
2026-08-29 10:44:22 +0530

    created http server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const http = require("http");
 const fs = require("fs");
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
   //processes incoming requests
   const log = `${Date.now()}: ${req.url}: New Request Received\n`;
+  if(req.url === "/favicon.ico") return res.end();
+  const parsed = url.parse(req.url, true);
+  console.log(parsed);
   fs.appendFile("log.txt", log, (err, data) => {
-    switch (req.url) {
+    switch (parsed.pathname) {
       case "/":
         res.end("home page");
         break;
       case "/about":
-        res.end("hey its me");
+        if(parsed.query !== null ) res.end(`hey its me, ${parsed.query.name}!`);
+        else res.end(`hey its me`);
         break;
       default:
         res.end('404 Not Found');
